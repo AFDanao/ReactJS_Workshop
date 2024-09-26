cfa11607643fc9de14c70011e927d2f9c035e82c
Gussi22 <samurai.adrian22@gmail.com>
2024-09-26 09:51:34 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let person = { name: "John Doe", age: 30, isActive: true };
+let numbers = [1, 2, 3, 4, 5];
+function greet() {
+  console.log("Hello World!");
+}
+let now = new Date();
+let pattern = /abc/;
